c97b9bd8dd5af20f765cd0f174e78a816b13117f
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-03-30 21:17:44 -0700

    Update settings

--- recipeplease/settings.py
+++ recipeplease/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = MY_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': DB_NAME,
         'USER': DB_USER,
         'PASSWORD': DB_PW,
-        'HOST': '',
+        'HOST': 'localhost',
         'PORT': '',
     }
 }
