094f6ee915a265426606afca2a57244fe681b76b
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-04-05 00:00:08 -0700

    Update settings

--- recipeplease/settings.py
+++ recipeplease/settings.py
@@ -139,8 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
+# STATIC_URL = '/staticfiles/'
 
 # Production Settings
 SESSION_COOKIE_SECURE = True
