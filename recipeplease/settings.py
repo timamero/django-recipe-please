e673ee638031f309d83a89cb16926aad0573ccd3
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-04-05 00:33:20 -0700

    Update settings

--- recipeplease/settings.py
+++ recipeplease/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = DEBUG_
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = [
 #   '127.0.0.1', 
