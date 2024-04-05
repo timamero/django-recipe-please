45dfcbdb92f83ac728ea4d0399bfc13623958795
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-04-04 23:18:57 -0700

    Update settings

--- recipeplease/settings.py
+++ recipeplease/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import  os
 
-from .secrets import *
+# from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +24,8 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = MY_SECRET_KEY
-# SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = MY_SECRET_KEY
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = DEBUG_
