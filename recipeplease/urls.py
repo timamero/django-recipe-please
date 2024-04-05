27e72f12dcf362119b73472c7913097959377f09
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-04-05 00:11:25 -0700

    Update url and create favicon folder

--- recipeplease/urls.py
+++ recipeplease/urls.py
@@ -15,16 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from django.contrib.staticfiles.storage import staticfiles_storage
-# from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += [
-    path('getrecipe/', include('getrecipe.urls'))
-    # path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
+    path('getrecipe/', include('getrecipe.urls')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
     # path('', include('getrecipe.urls'))
 ]
 
