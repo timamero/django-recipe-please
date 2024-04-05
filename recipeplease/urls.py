02e0da79412b0b181b5806638b1cf365babd32b1
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-04-05 00:06:58 -0700

    Add load staticfiles to template

--- recipeplease/urls.py
+++ recipeplease/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from django.contrib.staticfiles.storage import staticfiles_storage
+# from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,6 +24,7 @@ urlpatterns = [
 
 urlpatterns += [
     path('getrecipe/', include('getrecipe.urls'))
+    # path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
     # path('', include('getrecipe.urls'))
 ]
 
