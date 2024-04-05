e78ec97a7ea9947b3418cbb4acd5fecd9320ac8a
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-04-05 00:48:04 -0700

    Update settings

--- recipeplease/settings.py
+++ recipeplease/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'recipeplease.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        'NAME': BASE_DIR / "db.sqlite3",
+        'NAME': PROJECT_ROOT / "db.sqlite3",
     }
 }
 
