d29ba61e803f9aa6276f620c229f2da794ccee05
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-10-18 02:26:56 -0700

    Create recipe cache class

--- getrecipe/caches.py
+++ getrecipe/caches.py
@@ -1,14 +1,11 @@
 from django.core.cache import cache
 
-class UserProfileCache:
-    @staticmethod
-    def set_user_profile(user_id, profile_data, timeout=3600):
-        cache.set(f'user_profile_{user_id}', profile_data, timeout)
-
-    @staticmethod
-    def get_user_profile(user_id):
-        return cache.get(f'user_profile_{user_id}')
-
-    @staticmethod
-    def delete_user_profile(user_id):
-        cache.delete(f'user_profile_{user_id}')
+class ScrapedRecipeCache:
+    def __init__(self, url, title, ingredients, instructions, servings, prep_time, cook_time):
+        self.url = url
+        self.title = title
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.servings = servings
+        self.prep_time = prep_time
+        self.cook_time = cook_time
