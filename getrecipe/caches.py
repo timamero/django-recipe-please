967de274a225ecb5c70f2a458a42965301a99576
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-10-18 02:39:22 -0700

    Update index view to use cache

--- getrecipe/caches.py
+++ getrecipe/caches.py
@@ -9,3 +9,28 @@ class ScrapedRecipeCache:
         self.servings = servings
         self.prep_time = prep_time
         self.cook_time = cook_time
+
+
+# Serialize a Recipe instance to a dictionary
+def serialize_recipe(recipe):
+    return {
+        'url': recipe.url,
+        'title': recipe.title,
+        'ingredients': recipe.ingredients,
+        'instructions': recipe.instructions,
+        'servings': recipe.servings,
+        'prep_time': recipe.prep_time,
+        'cook_time': recipe.cook_time,
+    }
+
+# Deserialize a dictionary to a Recipe instance
+def deserialize_recipe(data):
+    return ScrapedRecipeCache(
+        url=data['url'],
+        title=data['title'],
+        ingredients=data['ingredients'],
+        instructions=data['instructions'],
+        servings=data['servings'],
+        prep_time=data['prep_time'],
+        cook_time=data['cook_time'],
+    )
--- getrecipe/views.py
+++ getrecipe/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.core.cache import cache
 
-from .models import ScrapedRecipe
+# from .models import ScrapedRecipe
+from .caches import ScrapedRecipeCache, serialize_recipe
 from .forms import AddRecipeForm
 
 from .recipe import Recipe
+
+import uuid
 
 def index(request):
     """View function for home page of site"""
@@ -19,17 +23,29 @@ def index(request):
         if form.is_valid():
             # Create new record
             get_recipe = Recipe(form.cleaned_data['url']) # create new instance of Recipe class
-            recipe_record = ScrapedRecipe(
-                url=get_recipe.recipe_url, 
-                title=get_recipe.title, 
-                ingredients=get_recipe.ingredients,
-                instructions=get_recipe.instructions,
-                servings=get_recipe.servings,
-                prep_time=get_recipe.preptime,
-                cook_time=get_recipe.cooktime
+            new_recipe = ScrapedRecipeCache(
+                get_recipe.recipe_url, 
+                get_recipe.title, 
+                get_recipe.ingredients,
+                get_recipe.instructions,
+                get_recipe.servings,
+                get_recipe.preptime,
+                get_recipe.cooktime
                 )
-            recipe_record.save()
-            return HttpResponseRedirect(reverse('recipe-detail', args=[str(recipe_record.id)]))
+            # recipe_record = ScrapedRecipe(
+            #     url=get_recipe.recipe_url, 
+            #     title=get_recipe.title, 
+            #     ingredients=get_recipe.ingredients,
+            #     instructions=get_recipe.instructions,
+            #     servings=get_recipe.servings,
+            #     prep_time=get_recipe.preptime,
+            #     cook_time=get_recipe.cooktime
+            #     )
+            # recipe_record.save()
+            recipe_id = str(uuid.uuid4())
+
+            cache.set(recipe_id, serialize_recipe(new_recipe), timeout=300)
+            return HttpResponseRedirect(reverse('recipe-detail', args=[str(recipe_id)]))
     # If this is a  GET (or any other method) create the default form
     else:
         form = AddRecipeForm()
@@ -39,7 +55,7 @@ def index(request):
     }
 
     # Clean up database (better to use a task queue or worker process, but will do it this way for now)
-    ScrapedRecipe.objects.all().delete()
+    # ScrapedRecipe.objects.all().delete()
 
     return render(request, 'index.html', context=context)
 
