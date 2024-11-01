bb1b019897ee107c02bd8c4e7a8957621d95811c
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-11-01 00:03:53 -0700

    chore: Remove disabled line

--- getrecipe/caches.py
+++ getrecipe/caches.py
@@ -2,7 +2,6 @@ class ScrapedRecipeCache:
     def __init__(
         self, url, title, ingredients, instructions, servings, prep_time, cook_time
     ):
-        # def __init__(self, url, title, ingredients, servings, prep_time, cook_time):
         self.url = url
         self.title = title
         self.ingredients = ingredients
