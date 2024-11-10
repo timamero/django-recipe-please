81f86b719023ca516851e3d42a2ad8343d05c95f
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-11-10 01:29:56 -0800

    chore: Clean up code

--- getrecipe/caches.py
+++ getrecipe/caches.py
@@ -2,18 +2,6 @@ import json
 
 from .services.recipe import ScrapedRecipe
 
-# class ScrapedRecipeCache:
-#     def __init__(
-#         self, url, title, ingredients, instructions, servings, prep_time, cook_time
-#     ):
-#         self.url = url
-#         self.title = title
-#         self.ingredients = ingredients
-#         self.instructions = instructions
-#         self.servings = servings
-#         self.prep_time = prep_time
-#         self.cook_time = cook_time
-
 
 # Serialize a Recipe instance to a dictionary
 def serialize_recipe(recipe):
