42259b9f9dd4067f61b1f982c831ae4a79e1af74
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-11-10 21:36:15 -0800

    build: Disable admin

--- getrecipe/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin  # noqa: F401
-
-# TO DO: Disable admin site
--- recipeplease/settings.py
+++ recipeplease/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
+    # "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
@@ -45,7 +45,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    # "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
