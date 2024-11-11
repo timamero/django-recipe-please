030c286f1267b1d144ccb43aac7bd812069562f9
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-11-10 21:39:07 -0800

    chore: Clean up code

--- recipeplease/urls.py
+++ recipeplease/urls.py
@@ -14,16 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# from django.contrib import admin
 from django.urls import path, include
 
 # Use static() to add url mapping to serve static files during development (only)
 from django.conf import settings
 from django.conf.urls.static import static
 
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-# ]
 
 urlpatterns = [
     path("", include("getrecipe.urls")),
