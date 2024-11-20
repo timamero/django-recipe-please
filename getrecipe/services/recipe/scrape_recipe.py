53c0a42ebff901e38fef2216cf79a8c0bd3ec3be
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-11-20 03:39:44 -0800

    Filter elements

--- getrecipe/services/recipe/scrape_recipe.py
+++ getrecipe/services/recipe/scrape_recipe.py
@@ -2,7 +2,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-from .scraper import get_elements_by_class_regex
+from scraper import get_elements_by_class_regex
 
 
 def get_soup(url):
@@ -135,11 +135,28 @@ def get_preptime(soup):
     if soup is None:
         return ""
     # TODO: Update regex so that it matches classes "recipes-details", "recipe-details", "recipedetails", "recipedetail"
-    # regex = re.compile(r"prep|recipe([s\-\_]{0,2})detail(s?)")
-    regex = re.compile(r"prep")
+    regex = re.compile(r"prep|recipe([s\-\_]{0,2})detail(s?)")
+    # regex = re.compile(r"prep")
     container = get_elements_by_class_regex(soup, regex)
 
     # TODO: Filter items to remove any that don't contain the text prep
+    for element in container:
+        # print(f"element: {element}")
+        has_digits = bool(re.search(r"\d", "".join(element.stripped_strings)))
+        prep_elements = element.find_all(string=re.compile(r"prep", re.I))
+
+        if has_digits and len(prep_elements) > 0:
+            # print(
+            #     f"element -find_all: {element.find_all(string=re.compile(r"prep", re.I))}"
+            # )
+            print(f"element: {"".join(element.stripped_strings)}")
+            match = re.search(
+                r"(\d{1,2})\s((min(utes)?)|(hour(s)?))",
+                "".join(element.stripped_strings),
+                re.I,
+            )
+            print(f"match: {match[0]}")
+            print("------")
 
     if len(container) == 0:
         return "Not found"
