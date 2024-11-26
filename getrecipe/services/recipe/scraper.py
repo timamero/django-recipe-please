a5fb0bb4638b8772dba1e1b53a405d3fb5e3bd60
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-11-26 02:09:53 -0800

    Improve function to get cook time

--- getrecipe/services/recipe/scraper.py
+++ getrecipe/services/recipe/scraper.py
@@ -25,7 +25,30 @@ def find_preparation_time(elements):
             )
             if match:
                 digit_pattern = r"\d\d?"
-                alpha_pattern = r"[a-zA-Z]++"
+                alpha_pattern = r"[a-zA-Z]+"
+                return "{0} {1}".format(
+                    re.search(digit_pattern, match[0]).group(),
+                    re.search(alpha_pattern, match[0]).group(),
+                )
+        return None
+
+
+def find_cook_time(elements):
+    for element in elements:
+        has_digits = bool(re.search(r"\d", "".join(element.stripped_strings)))
+        cook_elements = element.find_all(string=re.compile(r"cook", re.I))
+        for string in element.stripped_strings:
+            print(f"string: {string}")
+        if has_digits and len(cook_elements) > 0:
+            match = re.search(
+                r"(cook(ing|\s?time)?):?\s?(\d{1,2})\s?((min(utes)?)|(hour(s)?))",
+                "".join(element.stripped_strings),
+                re.I,
+            )
+            print(f"Match::: {match[0]}")
+            if match:
+                digit_pattern = r"\d\d?"
+                alpha_pattern = r"(min(utes)?)|(hour(s)?)"
                 return "{0} {1}".format(
                     re.search(digit_pattern, match[0]).group(),
                     re.search(alpha_pattern, match[0]).group(),
