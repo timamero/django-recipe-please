09f274adc5dcd0694ef15d036c9c70b3e2082e78
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-11-26 03:30:55 -0800

    Add headers to get request

--- getrecipe/services/recipe/scrape_recipe.py
+++ getrecipe/services/recipe/scrape_recipe.py
@@ -12,7 +12,10 @@ from .scraper import (
 
 def get_soup(url):
     """Get the content of the website"""
-    response = requests.get(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(url, headers=headers)
 
     if response.status_code != requests.codes.ok:
         return None
