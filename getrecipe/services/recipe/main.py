d3b4d41925aaa3788a8523fb55d701fb17f2cee6
Fatima Camero <54559570+timamero@users.noreply.github.com>
2024-11-30 23:24:24 -0800

    Rename file

--- getrecipe/services/recipe/main.py
+++ getrecipe/services/recipe/main.py
@@ -1,20 +1,20 @@
-from . import scrape_recipe
+from . import scraped_recipe
 from .model import ScrapedRecipe
 
 
 def get_scraped_recipe(url):
     try:
-        soup = scrape_recipe.get_soup(url)
+        soup = scraped_recipe.get_soup(url)
     except Exception as e:
         print(f"Error: {e}")
         return None
 
     return ScrapedRecipe(
         url=url,
-        title=scrape_recipe.title(soup),
-        ingredients=scrape_recipe.ingredients(soup),
-        instructions=scrape_recipe.instructions(soup),
-        servings=scrape_recipe.servings(soup),
-        prep_time=scrape_recipe.preptime(soup),
-        cook_time=scrape_recipe.cooktime(soup),
+        title=scraped_recipe.title(soup),
+        ingredients=scraped_recipe.ingredients(soup),
+        instructions=scraped_recipe.instructions(soup),
+        servings=scraped_recipe.servings(soup),
+        prep_time=scraped_recipe.preptime(soup),
+        cook_time=scraped_recipe.cooktime(soup),
     )
