7de491e8098520c7f476cda3283cc62a5b589718
Fatima Anne Camero <54559570+timamero@users.noreply.github.com>
2026-07-15 15:36:55 -0700

    Move return statement so errors are caught

--- getrecipe/services/recipe/main.py
+++ getrecipe/services/recipe/main.py
@@ -7,16 +7,17 @@ def get_scraped_recipe(url):
     try:
         soup = scraper.soup(url)
         recipe = scraped_recipe.recipe(soup)
+
+        return ScrapedRecipe(
+            url=url,
+            title=recipe["title"],
+            ingredients=recipe["ingredients"],
+            instructions=recipe["instructions"],
+            servings=recipe["servings"],
+            prep_time=recipe["preptime"],
+            cook_time=recipe["cooktime"],
+        )
+
     except Exception as e:
         print(f"Error: {e}")
         return None
-
-    return ScrapedRecipe(
-        url=url,
-        title=recipe["title"],
-        ingredients=recipe["ingredients"],
-        instructions=recipe["instructions"],
-        servings=recipe["servings"],
-        prep_time=recipe["preptime"],
-        cook_time=recipe["cooktime"],
-    )
